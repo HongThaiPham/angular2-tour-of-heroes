717a4002d67a234d645f4af18293bab55c820d5c
Leo Pham <hongthaipro@gmail.com>
2016-04-26 21:29:43 +0700

    lan 3

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
+}
+
+export class Hero {
+  id: number;
+  name: string;
+}
